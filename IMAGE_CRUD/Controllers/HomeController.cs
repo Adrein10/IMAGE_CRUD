a6fff320ad431fe2e71ae8f8cff491a1d28c36d5
asp <asp@aptech.com>
2024-06-08 16:57:32 +0500

    Added Complete Crud

--- IMAGE_CRUD/Controllers/HomeController.cs
+++ IMAGE_CRUD/Controllers/HomeController.cs
@@ -21,7 +21,22 @@ namespace IMAGE_CRUD.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var Srole = accessor.HttpContext.Session.GetString("sessionrole");
+            var Sname = accessor.HttpContext.Session.GetString("sessionname");
+            if(Srole != null && Srole == "Customer")
+            {
+                ViewBag.SessionName = Sname;
+                return View();
+            }
+            else if (Srole != null && Srole == "Admin")
+            {
+                ViewBag.SessionName = Sname;
+                return View("Index","Admin");
+            }
+            else
+            {
+                return RedirectToAction("login","Home");
+            }
         }
         public IActionResult productlist()
         {
@@ -58,7 +73,7 @@ namespace IMAGE_CRUD.Controllers
                     {
                         img.CopyTo(stream);
                     }
-                    var dbPath = Path.Combine("images", name);
+                    var dbPath = Path.Combine("productimg", name);
                     Product product = new Product()
                     {
                         ProductName = customProduct.CProduct.ProductName,
@@ -74,6 +89,77 @@ namespace IMAGE_CRUD.Controllers
                 {
                     ViewBag.imgfailed = "file type note supported";
                 }
+            }
+            return View();
+        }
+        public IActionResult deleteproduct(int id)
+        {
+            CustomProduct custom = new CustomProduct()
+            {
+                CProduct = context.Products.Find(id),
+                CPCategory = context.PCategories.ToList()
+            };
+            return View(custom);
+        }
+        [HttpPost]
+        public IActionResult deleteproduct(int id ,CustomProduct product)
+        {
+            context.Products.Remove(product.CProduct);
+            context.SaveChanges();
+            return RedirectToAction("productlist");
+        }
+        public IActionResult productedit(int id)
+        {
+            CustomProduct custom = new CustomProduct()
+            {
+                CProduct = context.Products.Find(id),
+                CPCategory = context.PCategories.ToList()
+            };
+            return View(custom);
+        }
+        [HttpPost]
+        public IActionResult productedit(int id,CustomProduct custom,IFormFile img)
+        {
+            var finduser = context.Products.FirstOrDefault(option => option.ProductId == id);
+            if (img != null && img.Length > 1)
+            {
+                var abc = 123;
+                var type = System.IO.Path.GetExtension(img.FileName).Substring(1);
+                if (type == "png" || type == "jfif" || type == "jpeg" || type == "jpg")
+                {
+                    var imgname = Path.GetFileName(img.FileName);
+                    var name = Guid.NewGuid() + imgname;
+                    var folder = Path.Combine(HttpContext.Request.PathBase.Value, "wwwroot/productimg");
+                    if (!Directory.Exists(folder))
+                    {
+                        Directory.CreateDirectory(folder);
+                    }
+                    var path = Path.Combine(folder, name);
+
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        img.CopyTo(stream);
+                    }
+                    var dbPath = Path.Combine("productimg", name);
+
+                    finduser.ProductName = custom.CProduct.ProductName;
+                    finduser.ProductImage = dbPath;
+                    finduser.ProductPrice = custom.CProduct.ProductPrice;
+                    finduser.ProductCategory = custom.CProduct.ProductCategory;
+                    context.SaveChanges();
+                    return RedirectToAction("productlist");
+                }
+                else
+                {
+                    ViewBag.imgfailed = "file type note supported";
+                }
+            }else if(img == null)
+            {
+                finduser.ProductName = custom.CProduct.ProductName;
+                finduser.ProductPrice = custom.CProduct.ProductPrice;
+                finduser.ProductCategory = custom.CProduct.ProductCategory;
+                context.SaveChanges();
+                return RedirectToAction("productlist");
             }
             return View();
         }
@@ -112,6 +198,29 @@ namespace IMAGE_CRUD.Controllers
         public IActionResult login(UserReg user)
         {
             var show = context.UserRegs.Where(option => option.Useremail == user.Useremail && option.Userpass == user.Userpass).FirstOrDefault();
+            
+            if (show != null)
+            {
+                var findrole = context.UserRoles.FirstOrDefault(option => option.RoleId == show.Userrole);
+                accessor.HttpContext.Session.SetString("sessionrole", findrole.RoleName);
+                accessor.HttpContext.Session.SetString("sessionname", show.Username);
+                if (findrole.RoleName == "Customer")
+                {
+                    return RedirectToAction("Index", "Home");
+                }else if(findrole.RoleName == "Admin")
+                {
+                    return RedirectToAction("Index", "Admin");
+                }
+            }
+            return View();
+        }
+        public IActionResult logout() 
+        {
+            if (accessor.HttpContext.Session.GetString("sessionname") != null)
+            {
+                accessor.HttpContext.Session.Clear();
+                return RedirectToAction("login");
+            }
             return View();
         }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
